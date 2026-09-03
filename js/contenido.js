dd2ff913a517e61b9a0c24c5b07768f07af43cc2
sarapericacho <pericachos@gmail.com>
2026-09-03 19:01:25 +0200

    Actualizar la web  03/09/2026 19:01

--- js/contenido.js
+++ js/contenido.js
@@ -1,7 +1,7 @@
 /* ==========================================================================
    CONTENIDO.JS  —  el contenido de tu web
    --------------------------------------------------------------------------
-   Generado desde el editor el 3/9/2026, 19:00:49
+   Generado desde el editor el 3/9/2026, 19:01:19
 
    Puedes editarlo aquí a mano o volver a usar el editor, como prefieras.
    Si lo tocas a mano: los textos van entre comillas ' ' y no borres las comas.
